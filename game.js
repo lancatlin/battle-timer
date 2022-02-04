83293d5b8121b9f57472daee0ea93d9c7946a581
wancat <wancat@wancat.cc>
2022-02-04 23:35:25 +0800

    Switch player

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ class Game {
     this.players_count = Number(param.get("players"))
     this.basic_time = Number(param.get("basic"))
     this.final_time = Number(param.get("final"))
+    this.current = 0
     
     this.players = new Array(this.players_count).fill(null).map((v, i) => ({
       id: i + 1,
@@ -16,6 +17,20 @@ class Game {
       final: this.final_time,
     }))
     console.log(this.players)
+  }
+
+  render() {
+    document.getElementById("player").innerHTML = `Player #${this.player.id}` 
+  }
+
+  nextPlayer() {
+    this.current += 1
+    this.current %= this.players_count
+    this.render()
+  }
+
+  get player() {
+    return this.players[this.current]
   }
 }
 
